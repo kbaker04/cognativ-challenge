6fbad7b08797e0b11f3b7b96218b954d134ec578
Ken <kbaker04@gmail.com>
2020-11-13 18:20:13 +1100

    fixed router import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import ProductPage from "./products/ProductPage";
 import PageNotFound from "./PageNotFound";
 import OfflinePage from "./maintenance/OfflinePage";
